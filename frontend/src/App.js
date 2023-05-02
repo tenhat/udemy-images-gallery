c3ee35d3fe4197eeea32d719b127b5d5dff4e020
tenjuhattori <tenjuhattori@gmail.com>
2023-05-02 09:04:18 +0900

    feat: front途中まで、backend実装開始

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import { useState } from "react";
 
 function App() {
+  const [word, setWord] = useState("");
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
+  console.log(word)
+
   return (
     <div className="App">
       <Header title="Image Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
